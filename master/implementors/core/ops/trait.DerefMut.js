5898e7ea0a47cd10db92298ddb7d006004f8c1a7
MaidSafe-QA <qa@maidsafe.net>
2016-03-03 21:05:50 +0000

    Updated documentation.

--- master/implementors/core/ops/trait.DerefMut.js
+++ master/implementors/core/ops/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['cbor'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.DerefMut.html' title='core::ops::DerefMut'>DerefMut</a> for <a class='struct' href='cbor/struct.CborBytes.html' title='cbor::CborBytes'>CborBytes</a>",];
+implementors['libc'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
